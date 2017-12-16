0fd68eb86bbe089b858635d000a4681cb207f287
Diogo Noda <diogonoda@hotmail.com>
2017-12-16 21:50:27 -0200

    Importing files and testes passing

--- spec/models/sale_spec.rb
+++ spec/models/sale_spec.rb
@@ -11,10 +11,17 @@ RSpec.describe Sale do
     expect(sale).not_to be_valid
   end
 
-  it "should be a valid sale record" do
+  it "should be valid" do
     sale.item_price = 10.0
     sale.purchase_count = 1
 
     expect(sale).to be_valid
   end
+
+  it "should calculate sale value" do
+    sale.item_price = 15.0
+    sale.purchase_count = 2
+
+    expect(sale.sale_value).to eq(30.0)
+  end 
 end
